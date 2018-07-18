8cca20ea384afd09b5af96cf7ec257f0cc2cb09b
javimar <javimardeveloper@gmail.com>
2018-07-18 18:58:03 +0200

    Add first array of places

---
+++ src/data/places.js
@@ -0,0 +1,9 @@
+export const places = [
+        {name: 'Alameda de Osuna Metro Station', location: { lat: 40.457862, lng: -3.587650 }},
+        {name: 'Alameda de Osuna Castle', location: { lat: 40.458940, lng: -3.592184 }},
+        {name: 'Capricho Metro Station', location: { lat: 40.453449, lng: -3.593985 }},
+        {name: 'Capricho Park', location: { lat: 40.454498, lng: -3.600616 }},
+        {name: 'Barajas Sports Center', location: { lat: 40.462812, lng: -3.587337 }},
+        {name: 'Gloria Fuertes Library', location: { lat: 40.460837, lng: -3.593630 }},
+        {name: 'Juan Carlos I Park Entrance', location: { lat: 40.461971, lng: -3.595013 }}
+    ];
